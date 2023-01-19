532bb24de271a4e4b3a4061e797eba02e51425f3
rivailJunior <rivail.rj@gmail.com>
2023-01-19 19:04:31 -0400

    sqlite database

---
+++ prisma/migrations/20230119230236_sqlite/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL
+);
