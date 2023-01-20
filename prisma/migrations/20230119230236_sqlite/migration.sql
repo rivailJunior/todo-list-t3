d8e2c07cd3dede01eaf140e5594b0688d0a508e1
rivailJunior <rivail.rj@gmail.com>
2023-01-19 20:10:30 -0400

    adding supabase as database

--- prisma/migrations/20230119230236_sqlite/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "name" TEXT NOT NULL
-);
